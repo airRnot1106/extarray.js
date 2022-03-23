9c10eb83b3060ad1530cbb87a516bbb14473be91
airRnot1106 <yngw.rintaro@gmail.com>
2022-03-23 16:16:10 +0900

    findにdocを追加

--- src/extarray.ts
+++ src/extarray.ts
@@ -241,6 +241,21 @@ export class Extarray<T> {
         );
     }
 
+    /**
+     * Returns the value of the first element in the extarray where predicate is
+     * true, and undefined otherwise.
+     *
+     * @memberof Extarray
+     * @param {(value: T, index: number, obj: T[]) => unknown} predicate Find
+     *   calls predicate once for each element of the extarray, in ascending
+     *   order, until it finds one where predicate returns true. If such an
+     *   element is found, find immediately returns that element value.
+     *   Otherwise, find returns undefined.
+     * @param {unknown} [thisArg] If provided, it will be used as the this value
+     *   for each invocation of predicate. If it is not provided, undefined is
+     *   used instead.
+     * @returns {any} {(T | undefined)}
+     */
     find(
         predicate: (value: T, index: number, obj: T[]) => unknown,
         thisArg?: unknown
